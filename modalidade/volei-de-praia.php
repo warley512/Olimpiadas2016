5f91fc8bb34c0c74d3daf5991b737b163517420b
warley512 <warley.peres@hotmail.com>
2016-03-18 00:59:01 -0300

    Atualização, volei e volei-de-praia

--- modalidade/volei-de-praia.php
+++ modalidade/volei-de-praia.php
@@ -1,8 +1,47 @@
 <?php
-$cabecalho_title = "título da pagina";
+$cabecalho_title = "$Volei-de-praia<";
 include("../include/cabecalho.php"); 
 ?>
 
-<!-- conteúdo da página -->
+<h1>Volei-de-praia</h1>
 
-<?php include("../include/rodape.php"); ?>
+<figure>
+	<img src="http://localhost:8080/images/volei-de-praia.jpg" alt="volei-de-praia"> 
+	<figcaption> Voleibol </figcaption>
+</figure>
+
+<P> - Variação natural do vôlei, a modalidade praticada na areia foi inventada em 1895,
+	nos Estados Unidos. Dispostos a praticar o esporte na praia, nas areias da Califórnia,
+	os pioneiros começaram a dar seus saques e cortadas em 1920. Aos poucos, a idéia foi
+	ganhando adeptos dentro e fora dos Estados Unidos, até que, em 1947, foi realizado
+	o primeiro torneio oficial de vôlei de praia. Três anos depois, veio o primeiro 
+	circuito, composto por etapas jogadas em cinco praias da Califórnia. </P>
+
+	<p>O sucesso da iniciativa motivou a fundação, em 1960, da California Beach Volleyball
+		Association (CBVA), que impulsionou a prática, consolidando a modalidade mundialmente
+		na década de 1980. Arrebatando diversos jogadores de quadra consagrados, 
+		o esporte ganhou, na Austrália, o primeiro circuito profissional. Em 1996,
+		nos Jogos de Atlanta, a modalidade tornou-se oficialmente um esporte olímpico.</p>
+
+		<P>No Brasil, a modalidade passou a ser praticada na década de 1930, de forma amadora,
+			nas praias de Copacabana e de Ipanema, no Rio de Janeiro. Durante as décadas seguintes,
+			o vôlei de praia ganhou milhares de adeptos no país, mas era encarado
+			como mera brincadeira de fim de semana.</P>
+
+			<p>Isso mudou a partir de 1986, quando se realizou o Hollywood Volley,
+				em Copacabana e em Santos (SP), com a participação de atletas brasileiros
+				e internacionais. Oficializado pela Federação Internacional de Vôlei (FIVB) 
+				após o Hollywood Volley, o vôlei de praia teve o seu primeiro Campeonato Mundial
+				disputado nas areias de Ipanema, em 1987. A dupla Sinjin Smith/Randy Stoklos,
+				dos Estados Unidos, conquistou o título, enquanto dois astros da Geração de
+				Prata do vôlei de quadra brasileiro,  Renan e Montanaro, foram os brasileiros
+				ma is bem colocados, terminando em terceiro lugar. Estava aberta a porta para
+				a evolução de uma modalidade que hoje ocupa posição de destaque no 
+				cenário esportivo brasileiro.</p>
+
+
+
+			</ul>
+		</div>
+
+		<?php include("../include/rodape.php"); ?>
